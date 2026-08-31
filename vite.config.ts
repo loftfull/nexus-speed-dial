65b7fb80e0c6666d1a5fe17405185324be45d878
loftfull <139651308+loftfull@users.noreply.github.com>
2026-09-01 00:25:10 +0300

    fix: type Vitest configuration with vitest config helper

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
@@ -8,6 +8,6 @@ export default defineConfig({
     setupFiles: './src/test/setup.ts',
     css: true,
     include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
-    exclude: ['src/**/*.node.test.ts'],
+    exclude: ['src/**/*.node.test.ts', 'e2e/**'],
   },
 });
