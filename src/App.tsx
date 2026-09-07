65ebed0ec849f91b23c0f5aef99126d9b26cd031
loftfull <139651308+loftfull@users.noreply.github.com>
2026-09-07 17:28:59 +0300

    refactor: render one Pure Speed Dial workspace

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
-import { AppShell } from './components/shell/AppShell.tsx';import { WorkspaceHeader } from './components/workspace/WorkspaceHeader.tsx';import { SpeedDialGrid } from './components/tiles/SpeedDialGrid.tsx';import { CalendarPopover } from './components/calendar/CalendarPopover.tsx';import { WeatherPopover } from './components/weather/WeatherPopover.tsx';import { TileSettingsPanel } from './components/settings/TileSettingsPanel.tsx';import { SectionContent } from './components/sections/SectionContent.tsx';import { SiteEditor } from './components/sites/SiteEditor.tsx';import { StructureEditor } from './components/structure/StructureEditor.tsx';import { TileStyleBridge } from './state/TileStyleBridge.tsx';import { useAppStore } from './state/useAppStore.ts';
-function WorkspaceContent(){const section=useAppStore(state=>state.section);if(section!=='home')return <SectionContent/>;return <><WorkspaceHeader/><SpeedDialGrid/></>}
-export default function App(){return <><TileStyleBridge/><AppShell><WorkspaceContent/></AppShell><CalendarPopover/><WeatherPopover/><TileSettingsPanel/><StructureEditor/><SiteEditor/></>}
+import { AppShell } from './components/shell/AppShell.tsx';
+import { WorkspaceHeader } from './components/workspace/WorkspaceHeader.tsx';
+import { SpeedDialGrid } from './components/tiles/SpeedDialGrid.tsx';
+import { CalendarPopover } from './components/calendar/CalendarPopover.tsx';
+import { WeatherPopover } from './components/weather/WeatherPopover.tsx';
+import { TileSettingsPanel } from './components/settings/TileSettingsPanel.tsx';
+import { SiteEditor } from './components/sites/SiteEditor.tsx';
+import { StructureEditor } from './components/structure/StructureEditor.tsx';
+import { TileStyleBridge } from './state/TileStyleBridge.tsx';
+
+export default function App() {
+  return <>
+    <TileStyleBridge/>
+    <AppShell>
+      <WorkspaceHeader/>
+      <SpeedDialGrid/>
+    </AppShell>
+    <CalendarPopover/>
+    <WeatherPopover/>
+    <TileSettingsPanel/>
+    <StructureEditor/>
+    <SiteEditor/>
+  </>;
+}
